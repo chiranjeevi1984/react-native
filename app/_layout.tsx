13ad34734086a25cf424ffae17264aa570b7ce8c
chiranjeevi1984 <kgchiranjeevi@gmail.com>
2026-06-03 10:17:13 +0530

    Updated the all files github

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import "@/global.css";
+import { Stack } from 'expo-router';
+import 'react-native-reanimated';
+
+
+export default function RootLayout() {
+  //const colorScheme = useColorScheme();
+  return (
+    <Stack />
+  );
+}
